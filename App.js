fddd79702c1bda372a19658e952ac473445d7903
kshitijk <gk.kshitij@gmail.com>
2022-06-14 13:18:07 +0530

    test update 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test update!!</Text>
+      <Text>test update 2!!</Text>
       <StatusBar style="auto" />
     </View>
   );
